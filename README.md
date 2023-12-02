70decf07cde49a51419882e1cd2d62a08227ff2f
Per Lundholm <per@artcomputer.se>
2023-12-02 15:10:22 +0100

    Solved day2, part 2.

--- README.md
+++ README.md
@@ -20,4 +20,5 @@ I had a try with Cursor, but it was a disappointment. GitHub Copilot results in 
 
 The first part was solved by Github Copilot without changes. I just copied the description and it worked.
 
+The second part was solved by Github Copilo as well, straight off. I am impressed.
 
--- src/main/java/se/artcomputer/aoc23/App2_1.java
+++ src/main/java/se/artcomputer/aoc23/App2_1.java
@@ -1,14 +1,17 @@
 package se.artcomputer.aoc23;
 
-import java.util.*;
-import java.util.regex.*;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class App2_1 {
     public static void main(String[] args) {
         try {
             String input = new String(Files.readAllBytes(Paths.get("data/input2.txt")));
-            System.out.println(sumOfPossibleGameIDs(input));
+            System.out.println("Part 1: " + sumOfPossibleGameIDs(input));
+            System.out.println("Part 2: " + sumOfPowersOfMinimumSets(input));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -47,6 +50,36 @@ public class App2_1 {
                 if (possible) {
                     sum += gameID;
                 }
+            }
+        }
+        return sum;
+    }
+
+    public static int sumOfPowersOfMinimumSets(String input) {
+        String[] games = input.split("\n");
+        Pattern gamePattern = Pattern.compile("Game (\\d+): (.+)");
+        Pattern setPattern = Pattern.compile("(\\d+) (red|green|blue)");
+        int sum = 0;
+        for (String game : games) {
+            Matcher gameMatcher = gamePattern.matcher(game);
+            if (gameMatcher.matches()) {
+                String[] sets = gameMatcher.group(2).split("; ");
+                int red = 0, green = 0, blue = 0;
+                for (String set : sets) {
+                    Matcher setMatcher = setPattern.matcher(set);
+                    while (setMatcher.find()) {
+                        int count = Integer.parseInt(setMatcher.group(1));
+                        String color = setMatcher.group(2);
+                        if (color.equals("red")) {
+                            red = Math.max(red, count);
+                        } else if (color.equals("green")) {
+                            green = Math.max(green, count);
+                        } else if (color.equals("blue")) {
+                            blue = Math.max(blue, count);
+                        }
+                    }
+                }
+                sum += red * green * blue;
             }
         }
         return sum;
